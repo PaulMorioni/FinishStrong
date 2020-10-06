762c419de69dd966a951c826d6183147a1c53498
PaulMorioni <morionipaul@gmail.com>
2020-10-06 14:50:55 -0500

    main application set up

---
+++ finish-strong-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { Header, List } from "semantic-ui-react";
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Header>Finish Strong</Header>
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
