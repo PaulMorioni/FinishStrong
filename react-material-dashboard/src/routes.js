dac10d44855e6555af300fffdf394b9dc6a15cbd
PaulMorioni <morionipaul@gmail.com>
2020-10-15 12:41:58 -0500

    Updated routes with new links to work with api

--- react-material-dashboard/src/routes.js
+++ react-material-dashboard/src/routes.js
@@ -3,13 +3,14 @@ import { Navigate } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/DashboardLayout';
 import MainLayout from 'src/layouts/MainLayout';
 import AccountView from 'src/views/account/AccountView';
-import CustomerListView from 'src/views/customer/CustomerListView';
+import TaskListView from 'src/views/tasks/TaskListView';
 import DashboardView from 'src/views/reports/DashboardView';
 import LoginView from 'src/views/auth/LoginView';
 import NotFoundView from 'src/views/errors/NotFoundView';
 import ProductListView from 'src/views/product/ProductListView';
 import RegisterView from 'src/views/auth/RegisterView';
 import SettingsView from 'src/views/settings/SettingsView';
+import UserListView from 'src/views/user/UserListView';
 
 const routes = [
   {
@@ -17,10 +18,11 @@ const routes = [
     element: <DashboardLayout />,
     children: [
       { path: 'account', element: <AccountView /> },
-      { path: 'customers', element: <CustomerListView /> },
+      { path: 'tasks', element: <TaskListView /> },
       { path: 'dashboard', element: <DashboardView /> },
       { path: 'products', element: <ProductListView /> },
       { path: 'settings', element: <SettingsView /> },
+      { path: 'users', element: <UserListView/> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
   },
