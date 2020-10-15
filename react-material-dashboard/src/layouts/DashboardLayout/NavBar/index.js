8bd516f12f01413dcb7adf72951b2afb81201c31
PaulMorioni <morionipaul@gmail.com>
2020-10-15 12:31:12 -0500

    Changed theme links to pages in sidebar

--- react-material-dashboard/src/layouts/DashboardLayout/NavBar/index.js
+++ react-material-dashboard/src/layouts/DashboardLayout/NavBar/index.js
@@ -37,14 +37,19 @@ const items = [
     title: 'Dashboard'
   },
   {
-    href: '/app/customers',
-    icon: UsersIcon,
-    title: 'Customers'
-  },
-  {
     href: '/app/products',
     icon: ShoppingBagIcon,
-    title: 'Products'
+    title: 'Projects'
+  },
+  {
+    href: '/app/tasks',
+    icon: UsersIcon,
+    title: 'Tasks'
+  },
+  {
+    href: '/app/users',
+    icon: UsersIcon,
+    title: 'Organization '
   },
   {
     href: '/app/account',
@@ -66,11 +71,6 @@ const items = [
     icon: UserPlusIcon,
     title: 'Register'
   },
-  {
-    href: '/404',
-    icon: AlertCircleIcon,
-    title: 'Error'
-  }
 ];
 
 const useStyles = makeStyles(() => ({
@@ -144,40 +144,6 @@ const NavBar = ({ onMobileClose, openMobile }) => {
             />
           ))}
         </List>
-      </Box>
-      <Box flexGrow={1} />
-      <Box
-        p={2}
-        m={2}
-        bgcolor="background.dark"
-      >
-        <Typography
-          align="center"
-          gutterBottom
-          variant="h4"
-        >
-          Need more?
-        </Typography>
-        <Typography
-          align="center"
-          variant="body2"
-        >
-          Upgrade to PRO version and access 20 more screens
-        </Typography>
-        <Box
-          display="flex"
-          justifyContent="center"
-          mt={2}
-        >
-          <Button
-            color="primary"
-            component="a"
-            href="https://react-material-kit.devias.io"
-            variant="contained"
-          >
-            See PRO version
-          </Button>
-        </Box>
       </Box>
     </Box>
   );
