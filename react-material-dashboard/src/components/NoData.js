cd9d5724028fff6953c14de781ca3e3d63ea0b71
PaulMorioni <morionipaul@gmail.com>
2020-11-18 16:55:30 -0600

    added component for SVG file for missing data on project page

---
+++ react-material-dashboard/src/components/NoData.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactComponent as NoDataSvg } from "./svgs/undraw_No_data_re_kwbl.svg";
+
+const NoData = (props) => {
+  return (
+    <NoDataSvg></NoDataSvg>
+  );
+};
+
+export default NoData;
